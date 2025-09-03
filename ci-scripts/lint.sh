b710f6bdf8879e3ea674dac6b089050582c16ba7
Augustine Abaris <augustin@bu.edu>
2025-09-03 13:03:49 -0400

    Try tofind galaxycollections

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -4,7 +4,6 @@ set -e
 source $PWD/ci-scripts/init.sh
 
 ${_PODMAN_RUN} \
-  -e ANSIBLE_COLLECTIONS_PATH="/code/collections:/code/galaxy-collections" \
   ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
   bash -l -c \
     ". /srv/docker-ansible/env/bin/activate;
