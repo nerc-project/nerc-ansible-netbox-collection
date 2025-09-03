aa2a1ff46a25d3fdd3579daa34a3271ab950e3d7
Augustine Abaris <augustin@bu.edu>
2025-09-03 16:00:26 -0400

    Test module paths

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -5,8 +5,10 @@ source $PWD/ci-scripts/init.sh
 
 ${_PODMAN_RUN} \
   ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
-  bash -l -c \
-    ". /srv/docker-ansible/env/bin/activate;
-     ansible-galaxy collection list
-     $GALAXY_INSTALL;
-     ansible-lint $@"
+    -e ANSIBLE_ROLES_PATH="/code/roles:/code/galaxy-roles" \
+    -e ANSIBLE_COLLECTIONS_PATH="/code/collections:/code/galaxy-collections" \
+    bash -l -c \
+      ". /srv/docker-ansible/env/bin/activate;
+      ansible-galaxy collection list
+      $GALAXY_INSTALL;
+      ansible-lint $@"
