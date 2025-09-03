45f4c32208b70d4ab81083ca00760a8af4051633
Augustine Abaris <augustin@bu.edu>
2025-09-03 12:41:49 -0400

    set collections path

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -5,6 +5,7 @@ source $PWD/ci-scripts/init.sh
 
 ${_PODMAN_RUN} \
   ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
+  -e ANSIBLE_COLLECTIONS_PATH="/code/collections:/code/galaxy-collections" \
   bash -l -c \
     ". /srv/docker-ansible/env/bin/activate;
      $GALAXY_INSTALL;
