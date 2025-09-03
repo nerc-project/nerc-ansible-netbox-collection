cc38b7c679930571e070e819a4b35c6bd0498f9e
Augustine Abaris <augustin@bu.edu>
2025-09-03 12:43:17 -0400

    Move env args

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -4,8 +4,8 @@ set -e
 source $PWD/ci-scripts/init.sh
 
 ${_PODMAN_RUN} \
-  ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
   -e ANSIBLE_COLLECTIONS_PATH="/code/collections:/code/galaxy-collections" \
+  ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
   bash -l -c \
     ". /srv/docker-ansible/env/bin/activate;
      $GALAXY_INSTALL;
