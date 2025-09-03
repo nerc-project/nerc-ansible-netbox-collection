ff6123b0c1d0471c6b27e8267aa984118d0926f3
Augustine Abaris <augustin@bu.edu>
2025-09-03 16:04:13 -0400

    kick 2

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -5,10 +5,11 @@ source $PWD/ci-scripts/init.sh
 
 ${_PODMAN_RUN} \
   -e ANSIBLE_ROLES_PATH="/code/roles:/code/galaxy-roles" \
-  -e ANSIBLE_COLLECTIONS_PATH="/code/collections:/code/galaxy-collections" \
+  -e ANSIBLE_COLLECTIONS_PATH="/code/collections:/code/galaxy-collections:/srv/docker-ansible/env/lib/python3.10/site-packages/ansible_collections" \
   ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
     bash -l -c \
       ". /srv/docker-ansible/env/bin/activate;
-      ansible-galaxy collection list
+      ansible-galaxy collection list;
+      ansible-galaxy role list;
       $GALAXY_INSTALL;
       ansible-lint $@"
