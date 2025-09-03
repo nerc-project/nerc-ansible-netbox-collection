68b468251c73f95f6d873feac0e129e03bd7dc97
Augustine Abaris <augustin@bu.edu>
2025-09-03 12:35:15 -0400

    Add extra modules

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -5,4 +5,7 @@ source $PWD/ci-scripts/init.sh
 
 ${_PODMAN_RUN} \
   ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
-  /srv/docker-ansible/env/bin/ansible-lint $@
+  bash -l -c \
+    ". /srv/docker-ansible/env/bin/activate;
+     $GALAXY_INSTALL;
+     ansible-lint $@
