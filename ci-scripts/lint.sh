2229936cdc2fbe6e1f494d4f2d7ab76e6f7537a9
Augustine Abaris <augustin@bu.edu>
2025-09-03 16:42:11 -0400

    Test new ansible build

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -4,12 +4,8 @@ set -e
 source $PWD/ci-scripts/init.sh
 
 ${_PODMAN_RUN} \
-  -e ANSIBLE_ROLES_PATH="/code/roles:/code/galaxy-roles" \
-  -e ANSIBLE_COLLECTIONS_PATH="/code/collections:/code/galaxy-collections:/srv/docker-ansible/env/lib/python3.10/site-packages/ansible_collections" \
-  ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
+  ghcr.io/nerc-project/nerc-ansible-container:sha-61be9eb \
     bash -l -c \
       ". /srv/docker-ansible/env/bin/activate;
-      ansible-galaxy collection list;
-      ansible-galaxy role list;
       $GALAXY_INSTALL;
       ansible-lint $@"
