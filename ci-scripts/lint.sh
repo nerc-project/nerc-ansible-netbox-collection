4dc254e2dc742c8ebbd165ab4b34db1b6d162f66
Augustine Abaris <augustin@bu.edu>
2025-09-03 13:07:10 -0400

    debug galaxy roles

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -7,5 +7,6 @@ ${_PODMAN_RUN} \
   ghcr.io/nerc-project/nerc-ansible-container:sha-11ab902 \
   bash -l -c \
     ". /srv/docker-ansible/env/bin/activate;
+     ansible-galaxy collection list
      $GALAXY_INSTALL;
      ansible-lint $@"
